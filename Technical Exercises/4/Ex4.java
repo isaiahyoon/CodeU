aacdb7effe3227b9fefacf9d1112b49d3621237f
Isaiah Yoon <isaiahyoon@berkeley.edu>
2015-05-25 09:46:06 -0700

    Finish Ex5

--- Technical Exercises/4/Ex4.java
+++ Technical Exercises/4/Ex4.java
@@ -1,10 +1,12 @@
-class Ex4 {
+class Ex5 {
 
-    public static int makeChange(int n, int[] m) {
-        
-    }
-
-    public static void main(String[] args) {
-        // Test cases
+    public static String didYouMean(String str) {
+        for (int i = 0; i < str.length(); i++) {
+            first = dictionary.get(str.substring(0, i));
+            if (first) {
+                return first + " " + str.substring(i+1);
+            }
+        }
+        return str;
     }
 }
